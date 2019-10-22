bb469b8d2157fe701b7166a784eb61be90fba59f
Michael Waldo <mbwcogic@yahoo.com>
2019-10-22 23:57:14 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,8 +8,13 @@ def second_challenge
       #your key/value pairs here!
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []#your key/value pairs here!
+      }
+    
+  }
 
   
 end
